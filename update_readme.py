9659d765fa370a6e1613e7a599d4d37682263ba1
fosdick.io <67963637+fosdickio@users.noreply.github.com>
2020-08-27 17:45:48 +0200

    Adding date of last update to the TILs.

--- update_readme.py
+++ update_readme.py
@@ -1,7 +1,9 @@
+from datetime import timezone
+import git
 import pathlib
+import re
 import sqlite_utils
 import sys
-import re
 
 
 ROOT_PATH = pathlib.Path(__file__).parent.resolve()
@@ -12,7 +14,40 @@ COUNT_REGEX = re.compile(r"<!\-\- Count starts \-\->.*<!\-\- Count ends \-\->", 
 COUNT_TEMPLATE = "<!-- Count starts -->{}<!-- Count ends -->"
 
 
-def build_database():
+def created_changed_times(repo_path, ref="main"):
+    created_changed_times = {}
+    repo = git.Repo(repo_path, odbt=git.GitDB)
+    commits = reversed(list(repo.iter_commits(ref)))
+    for commit in commits:
+        dt = commit.committed_datetime
+        affected_files = list(commit.stats.files.keys())
+        for filepath in affected_files:
+            correct_path = ""
+            if " => " in filepath:
+                chunks = filepath.split("/")
+                for chunk in chunks:
+                    if " => " in chunk:
+                        chunk = chunk.strip("{").strip("}").split(" => ")[1]
+                    correct_path += chunk + "/"
+                correct_path = correct_path[:-1]
+            else:
+                correct_path = filepath
+            if correct_path not in created_changed_times:
+                created_changed_times[correct_path] = {
+                    "updated": dt.isoformat(),
+                    "updated_utc": dt.astimezone(timezone.utc).isoformat(),
+                }
+            created_changed_times[correct_path].update(
+                {
+                    "updated": dt.isoformat(),
+                    "updated_utc": dt.astimezone(timezone.utc).isoformat(),
+                }
+            )
+    return created_changed_times
+
+
+def build_database(repo_path):
+    all_times = created_changed_times(repo_path)
     db = sqlite_utils.Database(ROOT_PATH / "til.db")
     table = db.table("til", pk="path")
     for filepath in ROOT_PATH.glob("*/*.md"):
@@ -22,13 +57,18 @@ def build_database():
         path = str(filepath.relative_to(ROOT_PATH))
         url = "https://github.com/fosdickio/til/blob/main/{}".format(path)
         record = {
-            "path": path.replace("/", "_"),
+            "path": path,
             "topic": path.split("/")[0],
             "title": title,
             "url": url,
-            "body": body,
+            "body": body
         }
-        table.insert(record)
+        if path in all_times:
+            record.update(all_times[path])
+        else:
+            raise RuntimeError("{} not found in all_time.".format(path))
+        with db.conn:
+            table.upsert(record, alter=True)
     if "til_fts" not in db.table_names():
         table.enable_fts(["title", "body"])
 
@@ -42,11 +82,7 @@ def update_readme():
     for topic, rows in by_topic.items():
         index.append("## {}\n".format(topic))
         for row in rows:
-            index.append(
-                "- [{title}]({url})".format(
-                    date=row["title"].split("T")[0], **row
-                )
-            )
+            index.append("- [{}]({}) ({})".format(row["title"], row["url"], row["updated"].split("T")[0]))
         index.append("")
     if index[-1] == "":
         index.pop()
@@ -63,5 +99,5 @@ def update_readme():
 
 
 if __name__ == "__main__":
-    build_database()
+    build_database(ROOT_PATH)
     update_readme()
